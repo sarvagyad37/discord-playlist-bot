cc0daf5153a4b9fe73f74847b3ce49dfd44f6076
sarvagya14503 <>
2021-05-21 10:27:23 +0000

    spotify int

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import discord
 import re
 from ytmusicapi import YTMusic
-#YTMusic.setup(filepath=headers_auth.json)
+
 ytmusic = YTMusic('headers_auth.json')
 
 client = discord.Client()
@@ -21,71 +21,75 @@ async def on_message(message):
     #msg = message.content
     if message.content.startswith('$ppls'):
         #main code
-        l=1000
+        l = 1000
         text_scraper = []
-        embedlist=[]
-        rawlinks=[]
-        #urls=[]
+        embedlist = []
+        rawlinks = []
+        rawnames=[]
+        songnames=[]
         async for msg in message.channel.history(limit=l):
-            if (msg.author.name)=="Rythm":
-              text_scraper.append([msg.content, msg.created_at])  
+            if (msg.author.name) == "Rythm":
+                text_scraper.append([msg.content, msg.created_at])
         #print(text_scraper)
         #print(" ")
 
         for i in range(l):
-          if re.match(r"^:thumbsup:",text_scraper[i][0]):
-            n=i
-            break
+            if re.match(r"^:thumbsup:", text_scraper[i][0]):
+                n = i
+                break
         #print(n)
         #print(text_scraper[n][1])
-        t1=text_scraper[n][1]
+        t1 = text_scraper[n][1]
         print(t1)
         #session1_text_scraper = text_scraper[:n]
         async for msg in message.channel.history(limit=l, after=t1):
-          if msg.embeds: #MIND BLOWING TECHNIQUE TO CHECK EMPTY LIST
-            embedlist.append(msg.embeds)
+            if msg.embeds:  #MIND BLOWING TECHNIQUE TO CHECK EMPTY LIST
+                embedlist.append(msg.embeds)
         #print(embedlist)
-        file1 = open("playlist.txt","w+")
-        #file2 = open("play_final.txt","w+")
+        file1 = open("playlist.txt", "w+")
+        file2 = open("play_final.txt","w+")
+
         for i in range(len(embedlist)):
-          temp=embedlist[i][0]
-          tempdesc=temp.description
-          if re.match("^\*", tempdesc):
-            tempurl = re.findall('(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-&?=%.]+', tempdesc)
-            url_ID = re.sub("/(https?)?+(:\/\/?)?+(www.?)?+[a-zA-Z]+(.com|.be)+(\/)+(watch?)?+[(?)]?+(v=?|V=?)?|(&)+(.*)/", "",tempurl)
-            print(url_ID)
-            rawlinks.append(url_ID)
+            temp = embedlist[i][0]
+            tempdesc = temp.description
+            if re.match("^\*", tempdesc):
+                #print(tempdesc)
+                tempname = re.findall('\[(.*?)\]',tempdesc)
+                tempurl = re.findall('(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-&?=%.]+',tempdesc)
+                print(tempurl)
+                #print(type(tempurl))
+                url = tempurl[0]
+                print(url)
+                parsed = url.split("=")
+                videoId = parsed[1]
+                rawlinks.append(videoId)
+                rawnames.append(tempname)
+        
         #print(rawlinks)
+        songnames = list(map(''.join, rawnames))
+        for i in range(len(songnames)):
+            file1.write(songnames[i])
+            file1.write('\n')
+
         urls = list(map(''.join, rawlinks))
         for i in range(len(urls)):
-          file1.write(urls[i])
-          file1.write('\n')
-        #file1.writelines(urls)
-        #raw_urls=[]
+            file2.write(urls[i])
+            file2.write('\n')
+  
         file1.close()
-        file1 = open("playlist.txt","r")
-        f = discord.File(file1)
-        await message.channel.send(file=f)
+        file2.close()
+        file1 = open("playlist.txt", "r")
+        f1 = discord.File(file1)
+        await message.channel.send(file=f1)
         file1.close()
+
+        file2 = open("play_final.txt", "r")
+        f2 = discord.File(file2)
+        await message.channel.send(file=f2)
+        file2.close()
+
         print("hogya")
 
-    if message.content.startswith('$pppls'):
-      l=11
-      embedlist=[]
-      async for msg in message.channel.history(limit=l):
-        if msg.embeds: #MIND BLOWING TECHNIQUE TO CHECK EMPTY LIST
-          embedlist.append(msg.embeds)
-      #print(embedlist)
-      for i in range(len(embedlist)):
-        temp=embedlist[i][0]
-        print(temp.title, temp.description)
-        
-
-
-    if message.content.startswith('$embed'):
-        embeds = message.embeds
-        for embed in embeds:
-          print(embed.to_dict())
     if message.content.startswith('thank you supriya'):
         await message.channel.send("SUPRIYA IS THE BESTTTT!!!!")
     if message.content.startswith('$plshelpmedha'):
@@ -94,7 +98,6 @@ async def on_message(message):
         await message.channel.send("sorry babe, i am still a work in progress")
     elif message.content.startswith('$plshelp '):
         await message.channel.send("sorry babe, i am still a work in progress")
-
 
 
 client.run(os.environ['TOKEN'])
